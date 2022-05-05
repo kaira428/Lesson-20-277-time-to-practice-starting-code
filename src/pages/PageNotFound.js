5210da53af39268a33c3e30af52144f50ae40c6e
Wong Keng <kaira428@gmail.com>
2022-05-05 18:04:01 +0800

    COMPLETE LECTURE 283.

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,5 @@
+const PageNotFound = () => {
+  return (<section className='centered'>Page Not Found</section>);
+};
+
+export default PageNotFound;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,22 @@
 import { Route, useParams } from 'react-router-dom';
 import Comments from '../components/comments/Comments'
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Keng', text: 'Learning React is fun!'},
+    {id: 'q2', author: 'WK', text: 'Learning React is great!'},
+];
 
 const QuoteDetail = () => {
 
     const param = useParams();
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === param.quoteId);
+
     return (
     <section>
-        <h1>Quote Detail Page</h1>
-        <h3>{`Quote ID: ${param.quoteId}`}</h3>
-        <Route path='/quotes/:quoteId/comments'>
+        <HighlightedQuote text={quote.text} author={quote.author}/>
+        <Route path={`/quotes/${quote.id}/comments`}>
             <Comments />
         </Route>
     </section>
