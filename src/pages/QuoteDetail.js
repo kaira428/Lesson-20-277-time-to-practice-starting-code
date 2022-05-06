85bb60787e009de2e60c9106b23f7a33568e9abf
Wong Keng <kaira428@gmail.com>
2022-05-06 17:58:36 +0800

    Complete Lecture 287.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,26 +1,37 @@
-import { Route, useParams } from 'react-router-dom';
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import { Route, useParams, useHistory } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Keng', text: 'Learning React is fun!'},
-    {id: 'q2', author: 'WK', text: 'Learning React is great!'},
+  { id: "q1", author: "Keng", text: "Learning React is fun!" },
+  { id: "q2", author: "WK", text: "Learning React is great!" },
 ];
 
 const QuoteDetail = () => {
+  const param = useParams();
+  const history = useHistory();
 
-    const param = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteId);
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === param.quoteId);
+  const addCommentHandler = () => {
+    history.push(`/quotes/${quote.id}/comments`);
+  };
 
-    return (
+  return (
     <section>
-        <HighlightedQuote text={quote.text} author={quote.author}/>
-        <Route path={`/quotes/${quote.id}/comments`}>
-            <Comments />
-        </Route>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${quote.id}`} exact>
+        <div>
+          <button className="centered btn" onClick={addCommentHandler}>
+            Load Comments
+          </button>
+        </div>
+      </Route>
+      <Route path={`/quotes/${quote.id}/comments`}>
+        <Comments />
+      </Route>
     </section>
-    );
+  );
 };
 
 export default QuoteDetail;
